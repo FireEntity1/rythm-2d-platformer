81ae14ad8c35c100c7369d28105d826c095824d1
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-06-25 15:29:16 -0600

    Attempt at laser detection

--- Scenes/Obstacles.gd
+++ Scenes/Obstacles.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
 var beat = 0
+var inLaser = false
+
+signal laser
+signal laserB
+signal laserM
+signal laserH
+signal laserL
+signal laserR
 
 func _ready():
 	pass
@@ -16,52 +24,55 @@ func _process(delta):
 	laserFireR(19.5)
 	laserFireL(19.5)
 	
-
-
-	
-	print(beat)
 	
 func laserFire(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.3
+	firebeat -= 1.2
 	if beat > firebeat and beat < firebeat + 1:
 		$Laser.play("default")
-		
-	
-	
 func laserFireL(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.5
+	firebeat -= 1
 	if beat > firebeat and beat < firebeat + 1:
 		$LaserL.play("default")
-		
+	if beat < firebeat+1 and beat < firebeat+2:
+		pass
 func laserFireR(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.5
+	firebeat -= 1
 	if beat > firebeat and beat < firebeat + 1:
 		$LaserR.play("default")
-
 func laserFireB(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.5
+	firebeat -= 1
 	if beat > firebeat and beat < firebeat + 1:
 		$LaserB.play("default")
-
 func laserFireM(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.5
+	firebeat -= 1
 	if beat > firebeat and beat < firebeat + 1:
 		$LaserM.play("default")
-		
 func laserFireH(firebeat):
 	if beat > firebeat + 2:
 		pass
-	firebeat -= 1.5
+	firebeat -= 1
 	if beat > firebeat and beat < firebeat + 1:
 		$LaserH.play("default")
 
+
+
+
+func _on_player_enter_laser(body):
+	if body == get_node("Player"):
+		print("entered!!")
+		inLaser = true
+
+
+func _on_player_exit_laser(body):
+	print("exited!!")
+	inLaser = false
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 600
 const JUMP_VELOCITY = -900
+var isLaser = false
+signal enterLaser(body)
+signal exitLaser(body)
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -10,16 +12,14 @@ func _process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	if Input.is_action_pressed("ui_left"):
 		$PlayerSprite.flip_h = 1
 		$PlayerCam.rotation_degrees = -10
@@ -28,10 +28,24 @@ func _process(delta):
 		$PlayerCam.rotation_degrees = 10
 	else:
 		$PlayerCam.rotation_degrees = 0
-		
 	if Input.is_action_just_pressed("ui_down"):
 		gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 10
 	if is_on_floor():
 		gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+	
+
 	move_and_slide()
+
+
+func _on_laser_body_entered(body):
+	if body == self:
+		isLaser = true
+		enterLaser.emit(body)
+		print(body)
+
+
+func _on_laser_body_exited(body):
+	if body == self:
+		exitLaser.emit(body)
+		isLaser = false
