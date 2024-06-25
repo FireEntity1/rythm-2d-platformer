347ca01e4ecc1177b2177bdaa4def9c383b7e5ea
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-06-24 19:49:14 -0600

    Basic Movement + Platforms

---
+++ Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 600
+const JUMP_VELOCITY = -700
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if Input.is_action_pressed("ui_left"):
+		$PlayerSprite.flip_h = 1
+		$PlayerCam.rotation_degrees = -10
+	elif Input.is_action_pressed("ui_right"):
+		$PlayerSprite.flip_h = 0
+		$PlayerCam.rotation_degrees = 10
+	else:
+		$PlayerCam.rotation_degrees = 0
+
+	move_and_slide()
